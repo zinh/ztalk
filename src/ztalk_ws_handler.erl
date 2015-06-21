4a0718e70a505a16b6942a9259da8c0784d32eff
Vinh Nguyen <xuanvinhct@gmail.com>
2015-06-21 15:43:54 +0000

    update web interface

--- src/ztalk_ws_handler.erl
+++ src/ztalk_ws_handler.erl
@@ -1,7 +1,7 @@
 -module(ztalk_ws_handler).
 -export([init/3, websocket_init/3, websocket_handle/3, websocket_info/3, websocket_terminate/3]).
 -record(state, {username, login = false}).
--define(WSKey, {pubsub, ws_socket}).
+-define(WSKey, {pubsub,wssocket}).
 
 init({tcp, http}, _Reg, _Opts) ->
   {upgrade, protocol, cowboy_websocket}.
@@ -12,12 +12,14 @@ websocket_init(tcp, Req, _Opts) ->
 
 websocket_handle({text, Msg}, Req, #state{login = false} = State) ->
   Login = Msg,
+  broadcast(<<Login/binary, " has joined">>),
   gproc:reg({p, l, ?WSKey}),
   NewState = State#state{username = Login, login = true},
   {reply, {text, <<"Hello ", Login/binary, "!">>}, Req, NewState};
 
 websocket_handle({text, Msg}, Req, #state{login = true} = State) ->
-  broadcast(State#state.username, Msg),
+  Login = State#state.username,
+  broadcast(<<Login/binary, ": ",  Msg/binary>>),
   {ok, Req, State}.
 
 websocket_info({_Pid, ?WSKey, Msg}, Req, State) ->
@@ -28,5 +30,5 @@ websocket_terminate(_Reason, _Req, _State) ->
   ok.
 
 %% Private functions
-broadcast(Name, Msg) ->
-  gproc:send({p, l, ?WSKey}, {self(), ?WSKey, <<Name/binary, ": ", Msg>>}).
+broadcast(Msg) ->
+  gproc:send({p, l, ?WSKey}, {self(), ?WSKey, Msg}).
